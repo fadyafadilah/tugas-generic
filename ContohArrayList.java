bb6d3588780f25e0742033e526736149f4fb2d69
fadyafadilah <114629375+fadyafadilah@users.noreply.github.com>
2024-04-15 07:58:47 +0700

    Add files via upload

---
+++ ContohArrayList.java
@@ -0,0 +1,25 @@
+package Sesi7;
+
+import java.util.ArrayList;
+
+public class ContohArrayList {
+
+    public static void main(String[] args) {
+ 
+        ArrayList<String> nama = new ArrayList<>();
+
+        nama.add("Muhamad");
+        nama.add("Fadya");
+        nama.add("Fadilah");
+
+        System.out.println("Isi ArrayList: " + nama);
+
+        nama.remove("Fadilah");
+
+        boolean adaFadya = nama.contains("Fadya");
+
+        System.out.println("Apakah ada Fadya? " + adaFadya);
+
+        System.out.println("Isi ArrayList setelah dihapus: " + nama);
+    }
+}
